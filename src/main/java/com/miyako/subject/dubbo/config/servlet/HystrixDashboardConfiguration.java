4782b83136a0003064532163e90c79f04bbd384c
lanxiao wei <weilanxiao@163.com>
2019-08-07 11:11:11 +0800

    初始化项目。。。

---
+++ src/main/java/com/miyako/subject/dubbo/config/servlet/HystrixDashboardConfiguration.java
@@ -0,0 +1,25 @@
+package com.miyako.subject.dubbo.config.servlet;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName HystrixDashboardConfiguration
+ * Description //TODO
+ * Author weila
+ * Date 2019-08-07-0007 11:09
+ */
+@Configuration
+public class HystrixDashboardConfiguration {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
